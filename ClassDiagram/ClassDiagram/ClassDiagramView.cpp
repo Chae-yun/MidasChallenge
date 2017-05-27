5aa3ed5cf87054a18285c089b82be5cd8a2eef77
Chae-yun <ycy6685@gmail.com>
2017-05-28 00:35:44 +0900

    saaaa

--- ClassDiagram/ClassDiagram/ClassDiagramView.cpp
+++ ClassDiagram/ClassDiagram/ClassDiagramView.cpp
@@ -250,7 +250,6 @@ void CClassDiagramView::OnMouseMove(UINT nFlags, CPoint point)
 void CClassDiagramView::OnLButtonDown(UINT nFlags, CPoint point)
 {
 	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
-	m_draw_mode = TRUE;
 	m_ptPrev = point;
 	AddDiagramList();
 	CView::OnLButtonDown(nFlags, point);
