3570b8ea7c5a4296620e2ed28b22e484d48798e3
민서 유 <min123seo@naver.com>
2018-05-26 13:03:52 +0900

    ㅇㅇ

--- ClassDiagram/ClassDiagram/resource.h
+++ ClassDiagram/ClassDiagram/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++에서 생성한 포함 파일입니다.
-// ClassDiagram.rc에서 사용되고 있습니다.
+// Microsoft Visual C++ generated include file.
+// Used by ClassDiagram.rc
 //
 #define IDI_ICON02                      5
 #define IDD_ABOUTBOX                    100
@@ -84,7 +84,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        312
-#define _APS_NEXT_COMMAND_VALUE         32816
+#define _APS_NEXT_COMMAND_VALUE         32818
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
